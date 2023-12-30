b292752d78d8a083d66d659c479fe95d9b90ed59
YaasineOuhadi <yassinouhadi@gmail.com>
2023-12-30 16:29:01 +0100

    Add test for generateRestfulAPIAction in CLI

---
+++ __tests__/generateRestfulAPI.test.ts
@@ -0,0 +1,19 @@
+// __tests__/generateRestfulAPI.test.ts
+import { join } from 'path'; // Import the 'join' function from the 'path' module
+import { generateRestfulAPIAction, GenerateOptions } from '../src/cli/index';
+
+describe('generateRestfulAPIAction', () => {
+  it('should generate Restful API code successfully', async () => {
+    // Use '__dirname' to get the directory of the current module (this test file)
+    const rootDir = join(__dirname, '..'); // Assuming the project structure, adjust as needed
+    const fileName = join(rootDir, 'example', 'test.jgen'); // Provide a valid relative file path
+    const options: GenerateOptions = { destination: join(rootDir, 'generated') }; // Provide a valid relative destination
+    
+    // Use try-catch to handle any potential errors during the test
+    try {
+      await generateRestfulAPIAction(fileName, options);
+    } catch (error) {
+      fail(error);
+    }
+  });
+});
