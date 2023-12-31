5aa9a8c62337e14de09d9d0a747710ee77b13c30
YaasineOuhadi <yassinouhadi@gmail.com>
2023-12-31 14:42:51 +0100

    Modify _tests_

--- __tests__/generateRestfulAPI.test.ts
+++ __tests__/generateRestfulAPI.test.ts
@@ -7,11 +7,11 @@ describe('generateRestfulAPIAction', () => {
     // Use '__dirname' to get the directory of the current module (this test file)
     const rootDir = join(__dirname, '..'); // Assuming the project structure, adjust as needed
     const fileName = join(rootDir, 'example', 'test.jgen'); // Provide a valid relative file path
-    const options: GenerateOptions = { destination: join(rootDir, 'generated') }; // Provide a valid relative destination
+    const options: GenerateOptions = { path: join(rootDir, '/example/test.jgen'), destination: join(rootDir, 'generated') }; // Provide a valid relative destination
 
     // Use try-catch to handle any potential errors during the test
     try {
-      await generateRestfulAPIAction(fileName, options);
+      await generateRestfulAPIAction(options);
     } catch (error) {
       // Replace fail(error) with an appropriate Jest assertion function
       expect(error).toBeFalsy(); // This is just an example, replace with your actual assertion
