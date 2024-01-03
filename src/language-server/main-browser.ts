07738ea6f77a374886c354eeaa68cfb960dca179
YaasineOuhadi <yassinouhadi@gmail.com>
2024-01-03 16:56:37 +0100

    Fixed tsconfig

--- src/language-server/main-browser.ts
+++ src/language-server/main-browser.ts
@@ -1,9 +1,8 @@
 import { EmptyFileSystem, startLanguageServer } from 'langium';
 import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser.js';
-// import {DedicatedWorkerGlobalScope} from 'typescript/lib/lib.webworker';
 import { createJgenServices } from './jgen-module';
 
-// declare const self: DedicatedWorkerGlobalScope;
+declare const self: DedicatedWorkerGlobalScope;
 
 const messageReader = new BrowserMessageReader(self);
 const messageWriter = new BrowserMessageWriter(self);
